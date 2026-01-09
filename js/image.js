22fe882438a3da69ee4c9d1592eead6729c40aab
BARATH P <barathsrm40@gmail.com>
2026-01-09 10:18:45 +0530

    Update image.js

--- js/image.js
+++ js/image.js
@@ -10,11 +10,17 @@ function translateImage(){
             .then(async result=>{
                 const text=result.data.text;
                 try{
-                    const res=await fetch("https://libretranslate.de/translate",{
+                    const res=await fetch("https://translate.argosopentech.com/translate",{
                         method:"POST",
-                        body:JSON.stringify({q:text,source:"en",target:translateTo.value,format:"text"}),
+                        body:JSON.stringify({
+                            q:text,
+                            source:"en",
+                            target:translateTo.value,
+                            format:"text"
+                        }),
                         headers:{"Content-Type":"application/json"}
                     });
+                    if(!res.ok) throw new Error("Network error");
                     const data=await res.json();
                     imgOutput.value=data.translatedText;
                 } catch(err){ imgOutput.value="Translation failed";}
